e13361844f6dc9f4cbb1fa8f1617a0c9123c0c01
zicoabhidey <dey.zico@gmail.com>
2025-07-28 11:06:33 -0400

    Switch CMS config from YAML to JavaScript

---
+++ admin/config.js
@@ -0,0 +1,43 @@
+window.CMS_MANUAL_INIT = true;
+
+document.addEventListener('DOMContentLoaded', function() {
+  window.CMS.init({
+    config: {
+      backend: {
+        name: 'github',
+        repo: 'zicoabhidey/test123',
+        branch: 'main'
+      },
+      media_folder: 'assets/uploads',
+      public_folder: '/assets/uploads',
+      collections: [
+        {
+          name: 'blog',
+          label: 'Blog Posts',
+          folder: '_posts',
+          create: true,
+          slug: '{{year}}-{{month}}-{{day}}-{{slug}}',
+          fields: [
+            {label: 'Layout', name: 'layout', widget: 'hidden', default: 'post'},
+            {label: 'Title', name: 'title', widget: 'string'},
+            {label: 'Publish Date', name: 'date', widget: 'datetime'},
+            {label: 'Body', name: 'body', widget: 'markdown'}
+          ]
+        },
+        {
+          name: 'pages',
+          label: 'Pages',
+          folder: '_pages',
+          create: true,
+          slug: '{{slug}}',
+          fields: [
+            {label: 'Layout', name: 'layout', widget: 'hidden', default: 'page'},
+            {label: 'Title', name: 'title', widget: 'string'},
+            {label: 'Permalink', name: 'permalink', widget: 'string'},
+            {label: 'Body', name: 'body', widget: 'markdown'}
+          ]
+        }
+      ]
+    }
+  });
+});
