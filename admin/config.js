4362b380eb85ffe0b2e1a9c4755ac1d7abf3059c
zicoabhidey <dey.zico@gmail.com>
2025-07-28 11:14:03 -0400

    Fix JavaScript CMS configuration

--- admin/config.js
+++ admin/config.js
@@ -1,43 +1,41 @@
-window.CMS_MANUAL_INIT = true;
+import CMS from 'https://unpkg.com/decap-cms@^3.0.0/dist/decap-cms.js'
 
-document.addEventListener('DOMContentLoaded', function() {
-  window.CMS.init({
-    config: {
-      backend: {
-        name: 'github',
-        repo: 'zicoabhidey/test123',
-        branch: 'main'
+CMS.init({
+  config: {
+    backend: {
+      name: 'github',
+      repo: 'zicoabhidey/test123',
+      branch: 'main'
+    },
+    media_folder: 'assets/uploads',
+    public_folder: '/assets/uploads',
+    collections: [
+      {
+        name: 'blog',
+        label: 'Blog Posts',
+        folder: '_posts',
+        create: true,
+        slug: '{{year}}-{{month}}-{{day}}-{{slug}}',
+        fields: [
+          { label: 'Layout', name: 'layout', widget: 'hidden', default: 'post' },
+          { label: 'Title', name: 'title', widget: 'string' },
+          { label: 'Publish Date', name: 'date', widget: 'datetime' },
+          { label: 'Body', name: 'body', widget: 'markdown' }
+        ]
       },
-      media_folder: 'assets/uploads',
-      public_folder: '/assets/uploads',
-      collections: [
-        {
-          name: 'blog',
-          label: 'Blog Posts',
-          folder: '_posts',
-          create: true,
-          slug: '{{year}}-{{month}}-{{day}}-{{slug}}',
-          fields: [
-            {label: 'Layout', name: 'layout', widget: 'hidden', default: 'post'},
-            {label: 'Title', name: 'title', widget: 'string'},
-            {label: 'Publish Date', name: 'date', widget: 'datetime'},
-            {label: 'Body', name: 'body', widget: 'markdown'}
-          ]
-        },
-        {
-          name: 'pages',
-          label: 'Pages',
-          folder: '_pages',
-          create: true,
-          slug: '{{slug}}',
-          fields: [
-            {label: 'Layout', name: 'layout', widget: 'hidden', default: 'page'},
-            {label: 'Title', name: 'title', widget: 'string'},
-            {label: 'Permalink', name: 'permalink', widget: 'string'},
-            {label: 'Body', name: 'body', widget: 'markdown'}
-          ]
-        }
-      ]
-    }
-  });
-});
+      {
+        name: 'pages',
+        label: 'Pages',
+        folder: '_pages',
+        create: true,
+        slug: '{{slug}}',
+        fields: [
+          { label: 'Layout', name: 'layout', widget: 'hidden', default: 'page' },
+          { label: 'Title', name: 'title', widget: 'string' },
+          { label: 'Permalink', name: 'permalink', widget: 'string' },
+          { label: 'Body', name: 'body', widget: 'markdown' }
+        ]
+      }
+    ]
+  }
+})
